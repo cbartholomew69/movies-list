405815b8f0baa08ec2aee19d4ad8231513a2b686
cbartholomew69 <bartholomew_christy@yahoo.com>
2018-01-31 15:30:49 -0500

    test for images

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -8,9 +8,13 @@ describe('First Test', () => {
   })
 })
 
-describe('check for the words Alice in Wonderland', () => [
+describe('check for the words Alice in Wonderland', () => {
   it('checks for text, Alice in Wonderland', () => {
     cy.visit('http://localhost:3000')
     cy.contains('Alice in Wonderland')
   })
-])
+
+  it('checks for img', () => {
+    cy.get('img').invoke('attr', 'https://images-na.ssl-images-amazon.com/images/M/MV5BODQwOTc5MDM2N15BMl5BanBnXkFtZTcwODQxNTEzNA@@._V1_SX300.jpg')
+  })
+})
